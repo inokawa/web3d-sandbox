3eb5c084c07fa14194c6e3ac54c4d86df43e59e4
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-01-15 00:31:25 +0900

    Hello three.js

---
+++ demo/index.js
@@ -0,0 +1,32 @@
+import * as THREE from 'three';
+
+function init(elem) {
+  const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+  camera.position.z = 1;
+
+  const scene = new THREE.Scene();
+
+  const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+  const material = new THREE.MeshNormalMaterial();
+
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  elem.appendChild(renderer.domElement);
+
+  animate()
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render(scene, camera);
+
+  }
+}
+
+export default init;
