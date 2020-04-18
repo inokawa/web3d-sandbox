31c3ad31fb34e72635fa7d2f9dcfcb6c3e829380
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-04-18 16:49:01 +0900

    Make it possible to use shaders to postprocess

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import * as THREE from "three";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
+import defaultVert from "./shaders/vert/default.vert";
+import defaultFrag from "./shaders/frag/default.frag";
 import { createCubeMesh } from "./utils/mesh";
 
-function init(elem) {
+function init(elem, { frag, vert } = {}) {
   const camera = new THREE.PerspectiveCamera(
     70,
     window.innerWidth / window.innerHeight,
@@ -18,6 +23,25 @@ function init(elem) {
   const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
 
+  // postprocessing
+  const composer = new EffectComposer(renderer);
+  composer.addPass(new RenderPass(scene, camera));
+
+  const effect = new ShaderPass(
+    new THREE.ShaderMaterial({
+      uniforms: {
+        tDiffuse: {
+          value: null,
+          type: "t",
+        },
+      },
+      vertexShader: vert || defaultVert,
+      fragmentShader: frag || defaultFrag,
+    })
+  );
+  composer.addPass(effect);
+
+  // render
   elem.appendChild(renderer.domElement);
 
   animate();
@@ -28,7 +52,7 @@ function init(elem) {
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.02;
 
-    renderer.render(scene, camera);
+    composer.render();
   }
 }
 
